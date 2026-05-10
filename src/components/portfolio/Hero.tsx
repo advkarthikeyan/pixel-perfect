1614a9438e0d8892cba2c26bda8a1582e4c50814
karthikeyan S <s.karthi11896@gmail.com>
2026-05-10 15:47:50 +0530

    Updated Hero.tsx

--- src/components/portfolio/Hero.tsx
+++ src/components/portfolio/Hero.tsx
@@ -283,8 +283,8 @@ const FloatingTag = ({
     secondary: "text-secondary",
     purple: "text-purple-400",
     amber: "font-mono-tag text-green-400",
-    pink: "font-mono-tag text-gradient italic animate-gradient text-violet-500",
-    rose: "font-mono-tag text-rose-500",
+    pink: "font-mono-tag text-violet-500",
+    rose: "font-mono-tag text-red-500",
   }[color];
   return (
     <div
